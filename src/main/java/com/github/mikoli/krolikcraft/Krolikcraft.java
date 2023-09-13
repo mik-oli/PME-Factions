e20affd4c0c2c7be2d1636468e1aa2149900e1af
Mik_oli <mikolajhain@gmail.com>
2023-09-14 00:44:03 +0200

    Staff whitelist variables saving/setting

--- src/main/java/com/github/mikoli/krolikcraft/Krolikcraft.java
+++ src/main/java/com/github/mikoli/krolikcraft/Krolikcraft.java
@@ -2,6 +2,7 @@ package com.github.mikoli.krolikcraft;
 
 import com.github.mikoli.krolikcraft.miningMonitor.MiningCounter;
 import com.github.mikoli.krolikcraft.miningMonitor.MiningMonitor;
+import com.github.mikoli.krolikcraft.staffWL.StaffWL;
 import com.github.mikoli.krolikcraft.utils.ConfigUtil;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -9,11 +10,21 @@ public final class Krolikcraft extends JavaPlugin {
 
     private final MiningCounter miningCounter = new MiningCounter();
     private final ConfigUtil configUtil = new ConfigUtil(this);
+    private final StaffWL staffWL = new StaffWL(this);
 
     @Override
     public void onEnable() {
         MiningMonitor miningMonitor = new MiningMonitor(this);
         this.saveDefaultConfig();
+
+        staffWL.loadData();
+    }
+
+    @Override
+    public void onDisable() {
+        staffWL.savaData();
+
+        this.saveConfig();
     }
 
     public MiningCounter getMiningCounter() {
@@ -22,5 +33,9 @@ public final class Krolikcraft extends JavaPlugin {
 
     public ConfigUtil getConfigUtil() {
         return this.configUtil;
+    }
+
+    public StaffWL getStaffWL() {
+        return this.staffWL;
     }
 }
---
+++ src/main/java/com/github/mikoli/krolikcraft/staffWL/StaffWL.java
@@ -0,0 +1,70 @@
+package com.github.mikoli.krolikcraft.staffWL;
+
+import com.github.mikoli.krolikcraft.Krolikcraft;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class StaffWL implements Listener {
+
+    private final Krolikcraft plugin;
+    private boolean whiteListStatus;
+    private final List<UUID> whitelistedPlayers = new ArrayList<>();
+
+    public StaffWL(Krolikcraft plugin) {
+        this.plugin = plugin;
+        this.loadData();
+    }
+
+    public boolean getWhiteListStatus() {
+        return this.whiteListStatus;
+    }
+
+    public void setWhiteListStatus(boolean status) {
+        this.whiteListStatus = status;
+    }
+
+    public List<UUID> getWhitelistedPlayers() {
+        return this.whitelistedPlayers;
+    }
+
+    public void addUserToList(Player player) {
+        this.whitelistedPlayers.add(player.getUniqueId());
+    }
+
+    public void removeUserToList(Player player) {
+        this.whitelistedPlayers.remove(player.getUniqueId());
+    }
+
+    public void loadData() {
+        this.loadWLStatus();
+        this.loadWhitelistedPlayers();
+    }
+
+    public void savaData() {
+        this.savaWLStatus();
+        this.saveWhitelistedPlayers();
+    }
+
+    private void loadWLStatus() {
+        whiteListStatus = this.plugin.getConfigUtil().getConfig().getBoolean("status");
+    }
+
+    private void loadWhitelistedPlayers() {
+        for (String s : this.plugin.getConfigUtil().getConfig().getStringList("whitelisted-users")) {
+            this.whitelistedPlayers.add(UUID.fromString(s));
+        }
+    }
+
+    private void savaWLStatus() {
+        this.plugin.getConfigUtil().getConfig().set("status", this.whiteListStatus);
+    }
+
+    private void saveWhitelistedPlayers() {
+        this.plugin.getConfigUtil().getConfig().set("whitelisted-users", this.whitelistedPlayers);
+    }
+}
