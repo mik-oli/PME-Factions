acc3b22ce6f1255ff693c598f27ba9ac4cee37b4
Mik_oli <mikolajhain@gmail.com>
2023-09-13 23:09:46 +0200

    Changed to single config file support

--- src/main/java/com/github/mikoli/krolikcraft/Krolikcraft.java
+++ src/main/java/com/github/mikoli/krolikcraft/Krolikcraft.java
@@ -2,18 +2,25 @@ package com.github.mikoli.krolikcraft;
 
 import com.github.mikoli.krolikcraft.miningMonitor.MiningCounter;
 import com.github.mikoli.krolikcraft.miningMonitor.MiningMonitor;
+import com.github.mikoli.krolikcraft.utils.ConfigUtil;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Krolikcraft extends JavaPlugin {
 
     private final MiningCounter miningCounter = new MiningCounter();
+    private final ConfigUtil configUtil = new ConfigUtil(this);
 
     @Override
     public void onEnable() {
         MiningMonitor miningMonitor = new MiningMonitor(this);
+        this.saveDefaultConfig();
     }
 
     public MiningCounter getMiningCounter() {
         return miningCounter;
+    }
+
+    public ConfigUtil getConfigUtil() {
+        return this.configUtil;
     }
 }
