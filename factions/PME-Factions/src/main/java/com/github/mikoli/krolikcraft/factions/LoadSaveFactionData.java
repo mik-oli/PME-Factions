f28cf08c2aa1aef755cb5fcd9c86a6f4b385824a
Mik_oli <mikolajhain@gmail.com>
2023-09-17 22:57:11 +0200

    Loading/saving factions data to file

---
+++ factions/PME-Factions/src/main/java/com/github/mikoli/krolikcraft/factions/LoadSaveFactionData.java
@@ -0,0 +1,38 @@
+package com.github.mikoli.krolikcraft.factions;
+
+import com.github.mikoli.krolikcraft.utils.FilesUtils;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Server;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+
+import java.util.HashSet;
+import java.util.UUID;
+
+public class LoadSaveFactionData {
+
+    public static void loadFactionData(FilesUtils file, Faction faction) {
+        YamlConfiguration dataFile = file.getData();
+        Server server = Bukkit.getServer();
+        faction.setName(dataFile.getString("name"));
+        faction.setLeader(server.getPlayer(dataFile.getString("leader")));
+
+        for (String s : dataFile.getStringList("members")) {
+            faction.addMember(server.getPlayer(s));
+        }
+    }
+
+    public static void saveFactionData(FilesUtils file, Faction faction) {
+        YamlConfiguration dataFile = file.getData();
+        Server server = Bukkit.getServer();
+        dataFile.set("name", faction.getName());
+        dataFile.set("leader", faction.getLeader().getUniqueId());
+
+        HashSet<UUID> membersToSave = new HashSet<>();
+        for (Player player : faction.getMembers()) {
+            membersToSave.add(player.getUniqueId());
+        }
+        dataFile.set("members", membersToSave);
+    }
+}
