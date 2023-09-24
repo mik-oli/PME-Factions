3416084c04888f30a87c07a6341c4d3137cbf8a7
Mik_oli <mikolajhain@gmail.com>
2023-09-24 21:21:01 +0200

    Adjusted block place event to disposal of persistent data of player

--- factions/PME-Factions/src/main/java/com/github/mikoli/krolikcraft/listeners/BlockPlaceListener.java
+++ factions/PME-Factions/src/main/java/com/github/mikoli/krolikcraft/listeners/BlockPlaceListener.java
@@ -4,18 +4,18 @@ import com.github.mikoli.krolikcraft.Krolikcraft;
 import com.github.mikoli.krolikcraft.factions.ClaimType;
 import com.github.mikoli.krolikcraft.factions.ClaimsManager;
 import com.github.mikoli.krolikcraft.factions.Faction;
-import com.github.mikoli.krolikcraft.utils.PersistentDataKeys;
+import com.github.mikoli.krolikcraft.factions.FactionsUtils;
 
 import org.bukkit.Chunk;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.UUID;
 
 public class BlockPlaceListener implements Listener {
 
@@ -30,13 +30,13 @@ public class BlockPlaceListener implements Listener {
         Block block = event.getBlock();
         if (block.getType() != Material.BEDROCK) return; //TODO changing bedrock to other block || checking if block has tag to claim
 
-        Player player = event.getPlayer();
-        if (!plugin.getFactionsHashMap().containsKey(plugin.getPersistentDataUtil().getPlayerData(player, PersistentDataKeys.FACTION))) return;
-        if (!Boolean.getBoolean(plugin.getPersistentDataUtil().getPlayerData(player, PersistentDataKeys.ISLEADER).toString())) return;
+        UUID playerUUID = event.getPlayer().getUniqueId();
+        if (!FactionsUtils.isPlayerInFaction(plugin, playerUUID)) return;
+        if (!FactionsUtils.getPlayersFaction(plugin, playerUUID).getLeader().equals(playerUUID)) return;
 
         ClaimsManager claimsManager = plugin.getClaimsManager();
         Chunk chunk = block.getChunk();
-        Faction faction = plugin.getFactionsHashMap().get(plugin.getPersistentDataUtil().getPlayerData(player, PersistentDataKeys.FACTION));
+        Faction faction = plugin.getFactionsHashMap().get(FactionsUtils.getPlayersFaction(plugin, playerUUID));
         if (!claimsManager.checkIfCanCreateClaim(faction, chunk, false)) return;
         Set<Chunk> chunksToClaim = new HashSet<>();
         chunksToClaim.add(chunk);
