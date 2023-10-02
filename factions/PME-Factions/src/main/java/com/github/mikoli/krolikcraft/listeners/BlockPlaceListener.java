9aff9d2e34d1d3af2aa7f3be5bee1f0d5d63eb5d
Mik_oli <mikolajhain@gmail.com>
2023-10-02 16:41:22 +0200

    Checking if placed block is claim flag to create claim

--- factions/PME-Factions/src/main/java/com/github/mikoli/krolikcraft/listeners/BlockPlaceListener.java
+++ factions/PME-Factions/src/main/java/com/github/mikoli/krolikcraft/listeners/BlockPlaceListener.java
@@ -5,9 +5,10 @@ import com.github.mikoli.krolikcraft.factions.ClaimType;
 import com.github.mikoli.krolikcraft.factions.ClaimsManager;
 import com.github.mikoli.krolikcraft.factions.Faction;
 import com.github.mikoli.krolikcraft.factions.FactionsUtils;
+import com.github.mikoli.krolikcraft.utils.BlockPersistentData;
+import com.github.mikoli.krolikcraft.utils.PersistentDataKeys;
 
 import org.bukkit.Chunk;
-import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -27,19 +28,25 @@ public class BlockPlaceListener implements Listener {
 
     @EventHandler
     public void onBlockPlaceEvent(BlockPlaceEvent event) {
+        //checking if placed block is claim flag
         Block block = event.getBlock();
-        if (block.getType() != Material.BEDROCK) return; //TODO changing bedrock to other block || checking if block has tag to claim
+        if (!BlockPersistentData.hasBlockData(plugin, PersistentDataKeys.CLAIMFLAG, block)) return;
+        if (!BlockPersistentData.getBlockData(plugin, PersistentDataKeys.CLAIMFLAG, block).equals("true")) return;
 
+        //checking if player is in faction and has ability to claim
         UUID playerUUID = event.getPlayer().getUniqueId();
         if (!FactionsUtils.isPlayerInFaction(plugin, playerUUID)) return;
-        if (!FactionsUtils.getPlayersFaction(plugin, playerUUID).getLeader().equals(playerUUID)) return;
+        if (!FactionsUtils.getPlayersFaction(plugin, playerUUID).getLeader().equals(playerUUID)) return; //TODO checking if player can claim
 
+        //claiming
         ClaimsManager claimsManager = plugin.getClaimsManager();
-        Chunk chunk = block.getChunk();
         Faction faction = FactionsUtils.getPlayersFaction(plugin, playerUUID);
+        Chunk chunk = block.getChunk();
         if (!claimsManager.checkIfCanCreateClaim(faction, chunk, false)) return;
         Set<Chunk> chunksToClaim = new HashSet<>();
         chunksToClaim.add(chunk);
         claimsManager.createClaim(faction, chunksToClaim, ClaimType.CLAIM);
+
+        BlockPersistentData.removeBlockData(plugin, PersistentDataKeys.CLAIMFLAG, block);
     }
 }
