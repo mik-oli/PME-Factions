3d964eefdd998e9958f59682a8ad737f92654cd4
Mik_oli <mikolajhain@gmail.com>
2023-11-27 20:55:10 +0100

    Item in inventory limiter

--- PME-Utilities/src/main/java/com/github/mikoli/pmeutilities/PMEUtilities.java
+++ PME-Utilities/src/main/java/com/github/mikoli/pmeutilities/PMEUtilities.java
@@ -1,9 +1,6 @@
 package com.github.mikoli.pmeutilities;
 
-import com.github.mikoli.pmeutilities.listeners.InvClickListener;
-import com.github.mikoli.pmeutilities.listeners.InvDragListener;
-import com.github.mikoli.pmeutilities.listeners.PlayerJoinListener;
-import com.github.mikoli.pmeutilities.listeners.SwapHandItemsListener;
+import com.github.mikoli.pmeutilities.listeners.*;
 import com.github.mikoli.pmeutilities.ringsMechanic.RingsManager;
 
 import org.bukkit.plugin.PluginManager;
@@ -18,6 +15,7 @@ public final class PMEUtilities extends JavaPlugin {
     private final InvDragListener invDragListener = new InvDragListener(this);
     private final PlayerJoinListener playerJoinListener = new PlayerJoinListener(this);
     private final InvClickListener invClickListener = new InvClickListener(this);
+    private final ItemPickUpListener itemPickUpListener = new ItemPickUpListener(this);
 
     @Override
     public void onEnable() {
@@ -40,5 +38,6 @@ public final class PMEUtilities extends JavaPlugin {
         pluginManager.registerEvents(invDragListener, this);
         pluginManager.registerEvents(playerJoinListener, this);
         pluginManager.registerEvents(invClickListener, this);
+        pluginManager.registerEvents(itemPickUpListener, this);
     }
 }
